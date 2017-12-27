ecac0ef7b6bd0e189d1ee1814f1de887daa68175
sevity <sevity@ymail.com>
2017-12-27 22:17:43 +0900

    keep asking close price every sec.

--- bithumb/eos_stoploss.py
+++ bithumb/eos_stoploss.py
@@ -1,24 +1,8 @@
 #! /usr/bin/env python
-# XCoin API-call sample script (for Python 3.X)
-#
-# @author	btckorea
-# @date	2017-04-11
-#
-#
-# First, Build and install pycurl with the following commands::
-# (if necessary, become root)
-#
-# https://pypi.python.org/pypi/pycurl/7.43.0#downloads
-#
-# tar xvfz pycurl-7.43.0.tar.gz
-# cd pycurl-7.43.0
-# python setup.py --libcurl-dll=libcurl.so install
-# python setup.py --with-openssl install
-# python setup.py install
-
 import sys
 from xcoin_api_client import *
 import pprint
+import time
 
 
 api_key = "api_connect_key";
@@ -31,37 +15,10 @@ rgParams = {
 	"payment_currency" : "KRW"
 };
 
-
-#
-# public api
-#
-# /public/ticker
-# /public/recent_ticker
-# /public/orderbook
-# /public/recent_transactions
-
-result = api.xcoinApiCall("/public/ticker", rgParams);
-print("status: " + result["status"]);
-print("last: " + result["data"]["closing_price"]);
-print("sell: " + result["data"]["sell_price"]);
-print("buy: " + result["data"]["buy_price"]);
-
-
-#
-# private api
-#
-# endpoint		=> parameters
-# /info/current
-# /info/account
-# /info/balance
-# /info/wallet_address
-
-#result = api.xcoinApiCall("/info/account", rgParams);
-#print("status: " + result["status"]);
-#print("created: " + result["data"]["created"]);
-#print("account id: " + result["data"]["account_id"]);
-#print("trade fee: " + result["data"]["trade_fee"]);
-#print("balance: " + result["data"]["balance"]);
-
-sys.exit(0);
+while True:
+    print("Bithumb Public API URI('/public/ticker') Request...");
+    result = api.xcoinApiCall("/public/ticker", rgParams);
+    print("- Status Code: " + result["status"]);
+    print("- Closing Price: " + result["data"]["closing_price"]);
+    time.sleep(1)
 

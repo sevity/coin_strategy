58a1df1e848c2e07b1b736eb7a4a66de81e65893
sevity <sevity@ymail.com>
2017-12-28 02:24:31 +0900

    we

--- bithumb/eos_stoploss.py
+++ bithumb/eos_stoploss.py
@@ -10,7 +10,6 @@ api_secret = "29119c1739d8e9451b461f5238353700";
 
 api = XCoinAPI(api_key, api_secret);
 
-
 def get_lastest_transaction(ticker):
 	rgParams = {
 	};
@@ -20,12 +19,13 @@ def get_lastest_transaction(ticker):
 			assert(result['status']=='0000')
 			a = result['data'][0]['transaction_date'];
 			b = 'uptick' if result['data'][0]['type'] == 'ask' else 'downtick';
-			c = result['data'][0]['price'];
+			c = int(result['data'][0]['price']);
 			return a, b, c
 		except:
 			# print("err1")
+			time.sleep(0.05)
 			pass
-		time.sleep(0.05)
+		
 
 
 def market_sell(ticker,cnt):
@@ -37,7 +37,7 @@ def market_sell(ticker,cnt):
 	result = api.xcoinApiCall("/trade/market_sell/", rgParams);
 	print(result)
 	assert(result['status']=='0000')
-	return result['data'][0]['price']
+	return int(result['data'][0]['price'])
 
 def order_new(ticker, price, cnt, askbid):
 	rgParams = {
@@ -52,7 +52,7 @@ def order_new(ticker, price, cnt, askbid):
 	return result['status']
 	# return result['data']['price']
 
-def one_turn():
+def one_turn(cnt):
 	eos_uc = 0  # eos uptick count
 	eos_lp = -1  # eos last price
 	eos_sp = -1  # eos start price
@@ -61,28 +61,36 @@ def one_turn():
 	while True:
 
 		date, updown, price = get_lastest_transaction('EOS')
+
 		if eos_lp == -1: eos_lp = price
 		if eos_sp == -1: eos_sp = price
 
 		if price > eos_lp: eos_uc += 1
-		else: eos_uc = 0;eos_sp = price
-		eos_lp = price
+		elif price < eos_lp: eos_uc = 0;eos_sp = price
 
 		print('EOS', date, "{:,}".format(int(price)), eos_uc)
+		time.sleep(0.5)
 
 		if eos_uc >= 3:
 			print('hit!!')
-			sell_price = market_sell('EOS', 1)
+			sell_price = market_sell('EOS', cnt)
 			print("sell price: ", sell_price)
-			print("order price: ", eos_sp)
+			# order_price = max(sell_price - 10, eos_sp)
+			order_price = eos_lp
+			if order_price >= sell_price: order_price = sell_price - 5
+			print("order price: ", order_price)
 
-			err = order_new('EOS', eos_sp, 1, 'bid')
+			total_sell = sell_price * cnt
+			new_cnt = round(1.0 * total_sell / order_price, 4) - 0.0001
+			print("new_cnt: ", new_cnt)
+
+			err = order_new('EOS', order_price, new_cnt, 'bid')
 			while err=='5600':  #please try again
 				print('trying again...')
-				err = order_new('EOS', eos_sp, 1, 'bid')
-
+				err = order_new('EOS', order_price, new_cnt, 'bid')
+				time.sleep(0.1)
 			return
+		eos_lp = price
 
 while True:
-	one_turn()
-	break
+	one_turn(20)

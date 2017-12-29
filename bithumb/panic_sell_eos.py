c252d9aad02e8e60bf5a1c0c81f5fb32614a75eb
sevity <sevity@ymail.com>
2017-12-29 11:50:52 +0900

    update

--- bithumb/panic_sell_eos.py
+++ bithumb/panic_sell_eos.py
@@ -4,120 +4,158 @@ from xcoin_api_client import *
 import pprint
 import time
 
+api_key = "";
+api_secret = "";
 
-api_key = "e3653ee4a58434abf5fba29f2797de9a";
-api_secret = "29119c1739d8e9451b461f5238353700";
+# options ########################################
+panic_sell_cnt = 10
+real_panic_sell_cnt = 30
+price_count_threshold = 4
+volume_count_threshold = 3
+eos_only_price_count_threshold = 7
+##################################################
 
-# api_key = "00665d4fb675b8bd94cbf747e8c75d5070";
-# api_secret = "7a429a1a656f2a153b21b83947484798";
+
 
 api = XCoinAPI(api_key, api_secret);
 
 def get_lastest_transaction(ticker):
-	rgParams = {
-	};
-	while True:
-		try:
-			result = api.xcoinApiCall("/public/recent_transactions/"+ticker, rgParams);
-			assert(result['status']=='0000')
-			a = result['data'][0]['transaction_date']
-			b = 'uptick' if result['data'][0]['type'] == 'ask' else 'downtick'
-			c = int(result['data'][0]['price'])
-                        d = float(result['data'][0]['units_traded'])
-			return a, b, c, d
-		except:
-                        print('e')
-			time.sleep(0.05)
-			pass
-		
+    rgParams = {
+    };
+    while True:
+        try:
+            result = api.xcoinApiCall("/public/recent_transactions/"+ticker, rgParams);
+            assert(result['status']=='0000')
+            a = result['data'][0]['transaction_date']
+            b = 'uptick' if result['data'][0]['type'] == 'ask' else 'downtick'
+            c = int(result['data'][0]['price'])
+            d = float(result['data'][0]['units_traded'])
+            return a, b, c, d
+        except:
+            print('e')
+            time.sleep(0.05)
+            pass 
 
 
 def market_sell(ticker,cnt):
-	rgParams = {
-		"units" : cnt,
-		"currency" : ticker
-
-	};
-	result = api.xcoinApiCall("/trade/market_sell/", rgParams);
-	# print(result)
-        if result['status'] != '0000':
-            return -1, result['status']
-	assert(result['status']=='0000')
-        fill_cnt = len(result['data'])
-        print('market sell fill_cnt', fill_cnt)
-        price = 0.0
-        for x in result['data']:
-            price += float(x['price'])
-
-	return price / fill_cnt, result['status']
+    rgParams = {
+        "units" : cnt,
+        "currency" : ticker
+    };
+    result = api.xcoinApiCall("/trade/market_sell/", rgParams);
+    if result['status'] != '0000': return -1, result['status']
+    assert(result['status']=='0000')
+    fill_cnt = len(result['data'])
+    print('market sell fill_cnt', fill_cnt)
+    price = 0.0
+    for x in result['data']:
+        price += float(x['price'])
+    return price / fill_cnt, result['status']
 
 def order_new(ticker, price, cnt, askbid):
-	rgParams = {
-		"order_currency" : ticker,
-		"payment_currency" : "KRW",
-		"units" : float(cnt),
-		"price" : int(price),
-		"type" : askbid,
-	};
-	result = api.xcoinApiCall("/trade/place/", rgParams);
-	# print(result)
-	return result['status']
-	# return result['data']['price']
+    rgParams = {
+        "order_currency" : ticker,
+        "payment_currency" : "KRW",
+        "units" : float(cnt),
+        "price" : int(price),
+        "type" : askbid,
+    };
+    result = api.xcoinApiCall("/trade/place/", rgParams);
+    # print(result)
+    return result['status']
+    # return result['data']['price']
+
+
+def panic_sell(sell_cnt):
+    sell_price, err = market_sell('EOS', sell_cnt)
+    while err != '0000':
+        if err != '5600': print('['+err+']')
+        assert(err=='5600')
+        sell_price, err = market_sell('EOS', sell_cnt)
+
+    print("panic sell done. sell price: ", sell_price)
+    return sell_price
+
+def buy_back(sell_price, sell_cnt, buy_price):
+    print("sell price", sell_price, "buy_price", buy_price)
+    assert(buy_price <= sell_price - 50)
+
+    sell_amount = sell_price * sell_cnt
+    fee = 0.00151  # trailing 1 is for preventing overflow
+    buy_cnt = round( (1.0) * sell_amount / (buy_price * (1.0 + fee) ), 4)# - 0.0001
+    print("buy_cnt: ", buy_cnt)
+
+    print('order_new...', buy_price, buy_cnt)
+    err = order_new('EOS', buy_price, buy_cnt, 'bid')
+    while err!='0000':  #please try again
+        if err != '5600':
+            print(err)
+            assert False
+        err = order_new('EOS', buy_price, buy_cnt, 'bid')
+        time.sleep(0.1)
+    print("buy_back done.")
+
+
+class Monitor:
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self.puc = 0   # continuous uptick count (price)
+        self.pdc = 0   # continuous downtick count (price)
+        self.vuc = 0  # continuous uptick count (volume)
+        self.vdc = 0  # continuous uptick count (volume)
+        self.lp = []  # list price
+        self.lv = []  # list volume
+
+    def get(self):
+        date, updown, price, volume = get_lastest_transaction(self.ticker)
+        while len(self.lp) > 0 and self.lp[-1] == price and self.lv[-1] == volume:
+            date, updown, price, volume = get_lastest_transaction(self.ticker)
+        self.lp.append(price)
+        self.lv.append(volume)
+        if len(self.lp) < 2: return False, 0, 0, 0, 0, 0, 0, 0, 0, 0
+
+        if   self.lp[-1] > self.lp[-2]: self.puc += 1; self.pdc = 0
+        elif self.lp[-1] < self.lp[-2]: self.puc = 0; self.pdc += 1
+
+        if   self.lv[-1] > self.lv[-2]: self.vuc += 1; self.vdc = 0
+        elif self.lv[-1] < self.lv[-2]: self.vuc = 0; self.vdc += 1
+
+        pd = self.lp[-1] - self.lp[-2]  # price delta
+        vd = self.lv[-1] - self.lv[-2]  # volume delta
+
+        return True, date, price, volume, pd, vd, self.puc, self.pdc, self.vuc, self.vdc
+
+
 
 def one_turn(cnt):
-	eos_dc = 0  # eos downtick count
-	eos_lp = -1  # eos last price
-        btc_dc = 0
-        btc_lp = -1
-        print_eos = False
-        print_btc = False
-        eos_lv = -1 # eos last volume
-        btc_lv = -1
-        eos_luc = 0
-        btc_luc = 0
+    eos_monitor = Monitor('EOS')
+    btc_monitor = Monitor('BTC')
 
-	while True:
+    while True:
+        ech, ed, ep, ev, epd, evd, epuc, epdc, evuc, evdc = eos_monitor.get()
+        bch, bd, bp, bv, bpd, bvd, bpuc, bpdc, bvuc, bvdc = btc_monitor.get()
 
-		date, updown, price, volume = get_lastest_transaction('EOS')
-                if price != eos_lp: print_eos = True
-		if eos_lp == -1: eos_lp = price
-                if eos_lv == -1: eos_lv = volume
-		if price < eos_lp: eos_dc += 1
-		elif price > eos_lp: eos_dc = 0
-                if volume > eos_lv: eos_luc += 1
-                elif volume < eos_lv: eos_luc = 0
+        if ech or bch:  # flag_eos_change or flag_btc_change
+            print(ed, 'EOS', "￦{:,}({:>+4,})".format(int(ep), epd), "￦↓{}".format(epdc), "V↑{}".format(evuc), "({0:>7.2f})".format(ev),\
+                  '\t\tBTC', "￦{:,}({:>+8,})".format(int(bp), bpd), "￦↓{}".format(bpdc), "V↑{}".format(bvuc), "({})".format(bv))
 
-		date, updown, bprice, bvolume = get_lastest_transaction('BTC')
-                if bprice != btc_lp: print_btc = True
-		if btc_lp == -1: btc_lp = bprice
-                if btc_lv == -1: btc_lv = bvolume
-		if bprice < btc_lp: btc_dc += 1
-		elif bprice > btc_lp: btc_dc = 0;
-                if bvolume > btc_lv: btc_luc += 1
-                elif bvolume < btc_lv: btc_luc = 0
-
-                if print_eos or print_btc:
-                    print(date, 'EOS', "{:,}".format(int(price)), eos_dc, eos_luc, volume, 'BTC', "{:,}".format(int(bprice)), btc_dc, btc_luc, bvolume)
-
-		if eos_dc >= 3 and btc_dc >=3:
-			print('panic warning!!')
-                        if eos_luc >=3 and btc_luc >=3:
-                            print('real panic!!!')
-                            assert(cnt > 0)
-                            sell_price, err = market_sell('EOS', cnt)
-                            while err != '0000':
-                                # print('['+err+']')
-                                assert(err=='5600')
-                                sell_price, err = market_sell('EOS', cnt)
-
-                            print("sell price: ", sell_price)
-		eos_lp = price
-                btc_lp = bprice
-                eos_lv = volume
-                btc_lv = bvolume
-                print_eos = False
-                print_btc = False
+        if epdc >= price_count_threshold and bpdc >= price_count_threshold:
+            if evuc >= volume_count_threshold and bvuc >= volume_count_threshold:
+                print('real panic!!!')
+                sell_price = panic_sell(real_panic_sell_cnt)
+                buy_price = sell_price - (epdc - price_count_threshold + 1) * 100
+                buy_back(sell_price, real_panic_sell_cnt, buy_price)
+            else:
+                print('small panic!!')
+                sell_price = panic_sell(panic_sell_cnt)
+                buy_price = sell_price - (epdc - price_count_threshold + 1) * 50
+                buy_back(sell_price, panic_sell_cnt, buy_price)
+        if epdc >= eos_only_price_count_threshold:
+                print('eos only panic!!')
+                sell_price = panic_sell(panic_sell_cnt)
+                buy_price = sell_price - (epdc - eos_only_price_count_threshold + 1) * 50
+                buy_back(sell_price, panic_sell_cnt, buy_price)
 
 while True:
-	one_turn(10)
-        break
+    one_turn(panic_sell_cnt)
+    break

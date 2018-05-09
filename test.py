823bc27d450e4d5226f8e929056e9754983de262
wikim <wi.kim@dco.com>
2018-05-09 22:31:05 +0900

    update

--- test.py
+++ test.py
@@ -6,19 +6,27 @@ bn = Coin('binance')
 print 'KRW info', bt.get_krw_info()
 print 'my coins', bt.get_balance_all()
 
+avg_gap = 0
 skip_turn = 0
+gap_sum = 0
+cnt = 0
 while True:
     a = bt.get_price('EOS', 'KRW')
     b = bn.get_price('EOS', 'BTC')
     c = bt.get_price('BTC', 'KRW')
     gap = b*c-a
-    print 'EOS price..', 'bithumb', a, 'binance', b*c, 'gap', gap
+    gap_sum += gap
+    cnt += 1
+    avg_gap = gap_sum / cnt
+
+    print 'EOS price..', 'bithumb', a, 'binance', b*c, 'gap', gap, 'avg_gap', avg_gap, 'adj_gap', gap - avg_gap
+    gap -= avg_gap
     if skip_turn > 0:
         skip_turn -= 1
-        if gap > 100:
+        if gap > 50:
             bt.market_buy('EOS', 1)
             skip_turn = 10
-        elif gap < -100:
+        elif gap < -50:
             bt.market_sell('EOS', 1)
             skip_turn = 10
     time.sleep(1)

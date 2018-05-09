e49e3b4d55c26899559f8f121fddf14c50e9fa6f
wikim <wi.kim@dco.com>
2018-05-09 17:19:56 +0900

    update

---
+++ binance/sevity_coin_api.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#! /usr/bin/env python
+from __future__ import absolute_import
+import sys
+import pprint
+import time
+
+from binance.client import Client
+from sevity_key import *
+
+
+api = Client(api_key, api_secret);
+

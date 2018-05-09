b58ce6fae387c9a08fa6e5ee7aa57ff67a76dfdf
wikim <wi.kim@dco.com>
2018-05-09 17:19:24 +0900

    update

--- coin.py
+++ coin.py
@@ -1,5 +1,13 @@
-from bithumb import sevity_key as bithumb_key
-from binance import sevity_key as binance_key
+import bithumb.sevity_coin_api
+import binance.sevity_coin_api
+
 class Coin:
-    def __init__(self):
-        pass
+    def __init__(self, exchange_name):
+        self._api = None
+        if exchange_name == 'bithumb':
+            self._api = bithumb.sevity_coin_api.api
+        elif exchange_name == 'binance':
+            pass
+            # self._api = binance.sevity_coin_api.api
+        else:
+            assert False

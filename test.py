52680f00447b9aac0225cc43bc4861c0038195a6
wikim <wi.kim@dco.com>
2018-05-10 09:42:26 +0900

    update

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
 from coin import *
 import time
+from datetime import datetime
+
+
+
+# param #######################################################################
+THRESHOLD = 50
+COOL_TIME = 10
+TRADE_CNT = 1
+###############################################################################
+
+
+
+
 bt = Coin('bithumb')
 bn = Coin('binance')
 
@@ -7,26 +20,33 @@ print 'KRW info', bt.get_krw_info()
 print 'my coins', bt.get_balance_all()
 
 avg_gap = 0
-skip_turn = 0
+skip_turn = 10
 gap_sum = 0
 cnt = 0
 while True:
-    a = bt.get_price('EOS', 'KRW')
-    b = bn.get_price('EOS', 'BTC')
-    c = bt.get_price('BTC', 'KRW')
+    try:
+        a = bt.get_price('EOS', 'KRW')
+        b = bn.get_price('EOS', 'BTC')
+        c = bt.get_price('BTC', 'KRW')
+    except Exception as e:
+        print('err', e)
+        time.sleep(1)
+        continue
     gap = b*c-a
     gap_sum += gap
     cnt += 1
     avg_gap = gap_sum / cnt
+    adj_gap = gap - avg_gap
 
-    print 'EOS price..', 'bithumb', a, 'binance', b*c, 'gap', gap, 'avg_gap', avg_gap, 'adj_gap', gap - avg_gap
-    gap -= avg_gap
-    if skip_turn > 0:
+    print datetime.now().strftime("%m-%d %H:%M:%S"), 'EOS price..', 'bithumb', a, 'binance', b*c, 'gap', gap, 'avg_gap', avg_gap, 'adj_gap', adj_gap
+    if skip_turn <= 0:
+        if adj_gap >= THRESHOLD:
+            bt.market_buy('EOS', TRADE_CNT)
+            skip_turn = COOL_TIME
+        elif adj_gap <= -THRESHOLD:
+            bt.market_sell('EOS', TRADE_CNT)
+            skip_turn = COOL_TIME
+    else:
         skip_turn -= 1
-        if gap > 50:
-            bt.market_buy('EOS', 1)
-            skip_turn = 10
-        elif gap < -50:
-            bt.market_sell('EOS', 1)
-            skip_turn = 10
+        print 'skip...'
     time.sleep(1)

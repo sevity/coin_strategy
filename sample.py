36e0d4ce8dd6c0ef8f2748bc33aa27c9f9fbf4cb
wikim <wi.kim@dco.com>
2018-12-07 14:00:24 +0900

    update

--- sample.py
+++ sample.py
@@ -2,14 +2,13 @@ from coin import *
 import time
 from datetime import datetime
 
-
 api_key = ""
 secret_key = ""
 
 coin = Coin('bithumb',api_key,secret_key)
 money = coin.get_asset_info('KRW')
-btc = coin.get_asset_info('BTC') # bitcoin
 xrp = coin.get_asset_info('XRP') # ripple
-print(money, btc, xrp)
+print('krw..', money)
+print('xrp..', xrp)
 
-bt.market_sell('XRP', 1)
+# coin.market_buy('XRP', 10)

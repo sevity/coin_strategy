4fff44ff8a54895d636a4616aa7b45ecb4c34a5b
sevity <sevity@ymail.com>
2020-04-01 20:24:05 +0900

    updown 업빗으로 포팅완료

--- exchange/upbit/sevity_coin_api.py
+++ exchange/upbit/sevity_coin_api.py
@@ -15,12 +15,12 @@ from urllib.parse import urlencode
 server_url = 'https://api.upbit.com'
 
 g_api_key = ""
-g_secret_key = ""
+g_api_secret = ""
 
 def set_key(api_key, secret_key):
     global g_api_key, g_api_secret
     g_api_key = api_key
-    g_secret_key = secret_key
+    g_api_secret = secret_key
 
 
 def get_price(ticker, currency):
@@ -40,16 +40,83 @@ def get_asset_info(currency):
         'nonce': str(uuid.uuid4()),
     }
 
-    jwt_token = jwt.encode(payload, g_secret_key).decode('utf-8')
+    jwt_token = jwt.encode(payload, g_api_secret).decode('utf-8')
     authorize_token = 'Bearer {}'.format(jwt_token)
     headers = {"Authorization": authorize_token}
     res = requests.get(url, headers=headers)
-
-    print(res.json())
-
-    rk = get_balance_info()
+    a = res.json()
     r = {}
-    r['total'] = round(float(rk['data']['total_' + currency.lower()]), 4)
-    r['inuse'] = round(float(rk['data']['in_use_' + currency.lower()]), 4)
-    r['free'] = round(float(rk['data']['available_' + currency.lower()]), 4)
+    for i in range(len(a)):
+        if a[i]['currency']==currency:
+            r['free'] = float(a[i]['balance'])
+            r['inuse'] = float(a[i]['locked'])
+            r['total'] = r['free'] + r['inuse']
+            break
     return r
+
+def order_new_sub(ticker, price, cnt, askbid):
+    rgParams = {
+        "order_currency" : ticker,
+        "payment_currency" : "KRW",
+        "units" : float(cnt),
+        "price" : int(price),
+        "type" : askbid,
+    };
+    while True:
+        try:
+            result = api.xcoinApiCall("/trade/place/", rgParams);
+            print_err(result)
+            r = int(result['status'])
+            # if r == 0: print(result)
+            m = None
+            if 'message' in result:
+                m = result['message']
+            if m is not None and m == '매수금액이 사용가능 KRW 를 초과하였습니다.':
+                r = -1
+            elif m is not None and m == '주문량이 사용가능 EOS을 초과하였습니다.':
+                r = -2
+            return r
+        except:
+            print('e')
+            time.sleep(0.05)
+            pass 
+
+
+def order_new(ticker, price, cnt, askbid, ord_type):
+    print('order_new...', ticker, price, cnt, askbid)
+    if ticker=='BTC':
+        price = round(price, -3) # minimum 1000 won
+
+    query = {
+        'market': 'KRW-{}'.format(ticker),
+        'side': askbid,
+        'volume': cnt,
+        'price': price,
+        'ord_type': ord_type,
+    }
+    query_string = urlencode(query).encode()
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': g_api_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, g_api_secret).decode('utf-8')
+    authorize_token = 'Bearer {}'.format(jwt_token)
+    headers = {"Authorization": authorize_token}
+
+    res = requests.post(server_url + "/v1/orders", params=query, headers=headers)
+    oid = json.loads(res.content)['uuid']
+    return oid
+
+def limit_buy(ticker, price, cnt):
+    return order_new(ticker, price, cnt, 'bid', 'limit')
+
+def limit_sell(ticker, price, cnt):
+    return order_new(ticker, price, cnt, 'ask', 'limit')
--- strategy/updown.py
+++ strategy/updown.py
@@ -8,10 +8,10 @@ from datetime import datetime
 
 
 # param #######################################################################
-THRESHOLD = 200
-COOL_TIME = 60 * 10
-TRADE_CNT = 1
-FEE = 0.002
+FEE = 0.003  # 수수료는 0.3%겠지?
+UPDOWN = 0.01  # 2% 상하로 걸어놓기..  성공하면 0.7%먹는 게임?
+BETTING = 50000  # 한번에 거는 돈의 크기
+COOL_TIME = 60 * 10  # 초단위
 ###############################################################################
 
 f = open("../upbit_api_key.txt", 'r')
@@ -27,31 +27,37 @@ cnt = 0
 
 while True:
     try:
-        a = coin.get_price('EOS', 'KRW')
-        b = coin.get_asset_info('KRW')
-        c = coin.get_asset_info('EOS')
+        a = coin.get_price('BTC', 'KRW')
+        money = coin.get_asset_info('KRW')
+        btc = coin.get_asset_info('BTC')
     except Exception as e:
         print('err', e)
         time.sleep(1)
         continue
 
-    print('KRW..', b)
-    print('EOS..', c)
-    print(datetime.now().strftime("%m-%d %H:%M:%S"), 'EOS price..', 'bithumb', a)
-    a = round(a, -1) # minimum 10 won
-    
-    if b['free'] > (a - THRESHOLD) * TRADE_CNT:
+    print('KRW..', money)
+    print('BTC..', btc)
+    print(datetime.now().strftime("%m-%d %H:%M:%S"), 'BTC price..', 'upbit', a)
+    #a = round(a, -1) # minimum 10 won
+
+
+    ask_price = a + a * UPDOWN;ask_price = round(ask_price, -3) # minimum 1000 won
+    bid_price = a - a * UPDOWN;bid_price = round(bid_price, -3) # minimum 1000 won
+    ask_cnt = float(BETTING) / ask_price 
+    bid_cnt = float(BETTING) / bid_price
+    if money['free'] > bid_price * bid_cnt :
         pass
     else:
         print('not enough KRW!')
         time.sleep(COOL_TIME)
         continue
 
-    if c['free'] > TRADE_CNT:
-        bt.limit_buy('EOS', a - THRESHOLD, TRADE_CNT)
-        bt.limit_sell('EOS', a + THRESHOLD, TRADE_CNT)
+    if btc['free'] > ask_cnt:
+        oid1 = coin.limit_buy('BTC', bid_price, bid_cnt)
+        oid2 = coin.limit_sell('BTC', ask_price, ask_cnt)
+        print("oid:", {oid1, oid2})
     else:
-        print('not enough EOS!')
+        print('not enough BTC!')
         time.sleep(COOL_TIME)
 
     time.sleep(COOL_TIME)
